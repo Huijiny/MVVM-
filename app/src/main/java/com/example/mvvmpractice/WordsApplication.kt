83ae066dda0ffa644681eba3e7419d6bfda18e1f
Huijiny <jiin20803@gmail.com>
2021-01-21 22:15:06 +0900

    feat: add NewWordActivity & connect with MainActivity

--- app/src/main/java/com/example/mvvmpractice/WordsApplication.kt
+++ app/src/main/java/com/example/mvvmpractice/WordsApplication.kt
@@ -7,7 +7,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
 
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
